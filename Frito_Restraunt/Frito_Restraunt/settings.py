8f574b2b71d4a89a21258f898f511b6cb695270c
unknown <alpha1.abdullah@gmail.com>
2024-05-30 23:32:51 +0300

    Huge Improvement

--- Frito_Restraunt/Frito_Restraunt/settings.py
+++ Frito_Restraunt/Frito_Restraunt/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fp%+_8@lq$sc3)y1yfh)1u9(z-(v&==(-9vwo*3cb#stex5n*_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*","http://www.frito-restraunt.com","192.168.1.4"]
 
@@ -69,6 +69,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'nocache.NoCache',
+
 
 ]
 
