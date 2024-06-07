8e3b8e2f23da2b1394d3ca650a3e3ff5d7e7eff8
unknown <alpha1.abdullah@gmail.com>
2024-06-07 22:55:23 +0300

    huge improve

--- Frito_Restraunt/static/assets/js/get_from_Card.js
+++ Frito_Restraunt/static/assets/js/get_from_Card.js
@@ -4,6 +4,19 @@ try {
         url: "/core/add-to-cart/",  // Update the URL here
         success: function(response) {
             console.log("Success:", response);
+            if(response.error=="User Empty"){
+                alert("user not logged in ")
+            } else if (response.success && Array.isArray(response.success) && response.success.length === 0) {
+                alert("Error: Success array is empty");
+            } else if (response.success && Array.isArray(response.success) && response.success.length > 0) {
+                alert("Success: Success array is not empty");
+                // Handle the response data here
+                console.log("Response success:", response.success);
+                // Further processing of response data
+            } else {
+                // Handle other possible cases or errors
+                console.log("Unexpected response structure:", response);
+            }
             // Handle the response data here
         },
         error: function(xhr, status, error) {
