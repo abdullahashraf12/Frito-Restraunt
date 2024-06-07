411b1cdf536d7eb3d7957cc3489af4a120053955
unknown <alpha1.abdullah@gmail.com>
2024-06-08 00:47:22 +0300

    huge improve

--- Frito_Restraunt/static/assets/js/get_from_Card.js
+++ Frito_Restraunt/static/assets/js/get_from_Card.js
@@ -1,4 +1,5 @@
 try {
+    total_for_all_products =0.00
     $.ajax({
         type: 'GET',
         url: "/core/add-to-cart/",  // Update the URL here
@@ -17,10 +18,12 @@ try {
                 for (const key in response.success) {
                     if (response.success.hasOwnProperty.call(response.success, key)) {
                         const element = response.success[key];
+                    
                         console.log("---------------------------")
                         console.log(element)
                         console.log("---------------------------")
                     if(element.user_meal_type == "Default"){
+                        total_for_all_products+=parseFloat(element.total_price_for_all)
                         $("#Add_Products_from_card_here").append(
                             `
                             <div class="card__Shopping_Card">
@@ -28,27 +31,31 @@ try {
         <div class="card__Shopping_Card-content">
             <h2>${element.product_name}</h2>
             <p>Quantity ${element.quantity}</p>
-            <p>All Product Type & SideDishes & Beverages Included <b style="color:green;">(Default)</b></p>
+            <p>Product Type & SideDishes & Beverages Included <b style="color:green;">(Default)</b></p>
         </div>
         <div class="card__Shopping_Card-price">
             <h5>Price <b style="color:red">${element.total_price_for_all} L.E</b></h5>
         </div>
-    </div>                 
+    </div>    
+                
            `
+           
                         );
                     }else{
+                        total_for_all_products+=parseFloat(element.total_price_for_all)
                         $("#Add_Products_from_card_here").append(
                             `
                             <div class="card__Shopping_Card">
         <img src="${element.default_image}" alt="Product Image" class="card__Shopping_Card-image">
         <div class="card__Shopping_Card-content">
             <h2>${element.product_name}</h2>
-            <p>All Product Type & SideDishes & Beverages Included <b style="color:green;">(Special Order)</b></p>
+            <p style="">Product Type & SideDishes & Beverages & Quantity Included <b style="color:green;">(Special Order)</b></p>
         </div>
         <div class="card__Shopping_Card-price">
             <h5>Price <b style="color:red">${element.total_price_for_all} L.E</b></h5>
         </div>
-    </div>                 
+    </div>
+              
            `);
                     }
 
@@ -57,6 +64,14 @@ try {
                         
                     }
                 }
+                $("#Add_Products_from_card_here").append(`<div style="display: flex; justify-content: space-between; padding-left: 10px; padding-right: 10px;">
+    <h4>Total Price:</h4>
+    <h5><b style="color:red; padding-right: 10px;">Price ${total_for_all_products} L.E</b></h5>
+</div>
+ `)
+ $("#Add_Products_from_card_here").append("<button style='width:100%; height:50px; color: white; background-color:red; font-weight:bold;'>Checkout</button>");
+ 
+
                 // Handle the response data here
                 console.log("Response success:", response.success);
                 // Further processing of response data
